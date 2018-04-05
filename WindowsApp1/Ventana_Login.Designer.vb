78044416819e269e7132c5f641b7068ed3e172cd
Matias-Barrios <soymatiasbarrios@gmail.com>
2018-04-05 10:54:03 -0500

    Colors

--- WindowsApp1/Ventana_Login.Designer.vb
+++ WindowsApp1/Ventana_Login.Designer.vb
@@ -23,56 +23,58 @@ Partial Class Ventana_Login
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtNombreUsuario = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Verdana", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(24, 154)
+        Me.Label2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(242, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(242, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(36, 133)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(208, 42)
+        Me.Label2.Size = New System.Drawing.Size(185, 40)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Password"
         Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'TextBox1
+        'txtNombreUsuario
         '
-        Me.TextBox1.Location = New System.Drawing.Point(31, 105)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
-        Me.TextBox1.Size = New System.Drawing.Size(201, 20)
-        Me.TextBox1.TabIndex = 2
+        Me.txtNombreUsuario.Location = New System.Drawing.Point(31, 105)
+        Me.txtNombreUsuario.Name = "txtNombreUsuario"
+        Me.txtNombreUsuario.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
+        Me.txtNombreUsuario.Size = New System.Drawing.Size(201, 20)
+        Me.txtNombreUsuario.TabIndex = 2
         '
-        'TextBox2
+        'txtPassword
         '
-        Me.TextBox2.Location = New System.Drawing.Point(31, 211)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
-        Me.TextBox2.Size = New System.Drawing.Size(201, 20)
-        Me.TextBox2.TabIndex = 3
+        Me.txtPassword.Location = New System.Drawing.Point(31, 178)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
+        Me.txtPassword.Size = New System.Drawing.Size(201, 20)
+        Me.txtPassword.TabIndex = 3
         '
-        'Button1
+        'btnLogin
         '
-        Me.Button1.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(31, 252)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(201, 39)
-        Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Log in!"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnLogin.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLogin.Location = New System.Drawing.Point(31, 252)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(201, 39)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "Log in!"
+        Me.btnLogin.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Verdana", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(24, 49)
+        Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(242, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(242, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(36, 62)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(215, 42)
+        Me.Label1.Size = New System.Drawing.Size(193, 40)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Username"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -81,10 +83,11 @@ Partial Class Ventana_Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(14, Byte), Integer), CType(CType(214, Byte), Integer), CType(CType(236, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(262, 313)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtNombreUsuario)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Ventana_Login"
@@ -94,8 +97,8 @@ Partial Class Ventana_Login
 
     End Sub
     Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents txtNombreUsuario As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnLogin As Button
     Friend WithEvents Label1 As Label
 End Class
--- WindowsApp1/Ventana_Login.vb
+++ WindowsApp1/Ventana_Login.vb
@@ -5,51 +5,39 @@
     Private usersList As New List(Of User)
 
 
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-
-    End Sub
-
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         usersList.Add(New User("Matias", "Barrios"))
         usersList.Add(New User("Gimena", "Barrios"))
-        Button1.Enabled = False
+        btnLogin.Enabled = False
         centrarVentana(Me)
 
 
     End Sub
 
+    Private Sub txtNombreUsuario_TextChanged(sender As Object, e As EventArgs) Handles txtNombreUsuario.TextChanged
 
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-
-        If TextBox1.Text = "" Or TextBox2.Text = "" Then
-            Button1.Enabled = False
+        If txtNombreUsuario.Text = "" Or txtPassword.Text = "" Then
+            btnLogin.Enabled = False
         Else
-            Button1.Enabled = True
-
-
+            btnLogin.Enabled = True
         End If
     End Sub
-    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+    Private Sub txtPassword_TextChanged(sender As Object, e As EventArgs) Handles txtPassword.TextChanged
 
 
-        If TextBox1.Text = "" Or TextBox2.Text = "" Then
-            Button1.Enabled = False
+        If txtNombreUsuario.Text = "" Or txtPassword.Text = "" Then
+            btnLogin.Enabled = False
         Else
-            Button1.Enabled = True
+            btnLogin.Enabled = True
 
 
         End If
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If Validar(usersList, Me.TextBox1.Text, Me.TextBox2.Text) Then
-            WindowsApp1.Ventana_Principal.Show()
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        If Validar(usersList, Me.txtNombreUsuario.Text, Me.txtPassword.Text) Then
+            Ventana_Principal.Show()
             Me.Hide()
         Else
             MsgBox("Either your username or your password is incorrect", 0, "Authentication error")
