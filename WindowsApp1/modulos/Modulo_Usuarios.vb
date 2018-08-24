bd1ee64c6e01bc7ecc68fb711af668387c2fdf93
clferreri <clferreri@SUY00NBTEC11>
2018-08-23 22:16:58 -0300

    mas

--- WindowsApp1/modulos/Modulo_Usuarios.vb
+++ WindowsApp1/modulos/Modulo_Usuarios.vb
@@ -1,7 +1,21 @@
 ﻿Module Modulo_Usuarios
     Public Class User
 
-        Public Property Name As String
+        Public Property CI As String
+        Public Property primer_nombre As String
+        Public Property segundo_nombre As String
+        Public Property primer_apellido As String
+        Public Property segundo_apellido As String
+        Public Property fecha_nacimiento As Date
+        Public Property email As String
+        Public Property grado As String
+        Public Property hace_proyecto As Boolean
+        Public Property nota_final_pro As Boolean
+        Public Property juicio_final As String
+        Public Property tipo As String
+
+
+
 
         Public Property Password As String
         Public Property Rol As String
@@ -11,6 +25,8 @@
             Me.Password = password
             Me.Rol = rol
         End Sub
+
+
 
     End Class
 
